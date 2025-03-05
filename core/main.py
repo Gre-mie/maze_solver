a0d0c99347db8c8dda92a63f720ec21b960886d6
Gre-mie <meow31-cat@outlook.com>
2025-03-05 07:21:32 +0000

    Add: Cell class

--- core/main.py
+++ core/main.py
@@ -1,8 +1,9 @@
 # main.py, runs the programme
 
 from objects.window import Window
-from objects.line import Line
 from objects.point import Point
+from objects.cell import Cell
+
 
 def main():
     
@@ -14,34 +15,47 @@ def main():
 
 
     # vvv temp code block vvv
+    # !!! No longer works due to no relevent imports !!!
+#    # practice drawing lines
+ #   print(f"window: width: {window.width}  height: {window.height}")
+  #  padding = 1.5
+   # num_of_boxes = 10
+    #line_width = (window.width - padding * 2) / num_of_boxes
+#    line_height = (window.height - padding * 2) / num_of_boxes
+ #   w = padding
+  #  h = padding
+   # last_w = w + line_width
+    #last_h = h + line_height
+#    while h <= window.height:
+ #       while w <= window.width:
+  #          line1 = Line(Point(last_w, h), Point(w, h))
+   #         line2 = Line(Point(w, last_h), Point(w, h))
+    #        print(f"L1:  p1: x: {line1.p1.x}  y: {line1.p1.y}  p2: x: {line1.p2.x}  y: {line1.p2.y}")
+     #       print(f"L1:  p1: x: {line2.p1.x}  y: {line2.p1.y}  p2: x: {line2.p2.x}  y: {line2.p2.y}")
+      #      window.draw_line(line1, "black")
+       #     window.draw_line(line2, "red")
 
-    # practice drawing lines
-    print(f"window: width: {window.width}  height: {window.height}")
-    padding = 10
-    num_of_boxes = 10
-    line_width = (window.width - padding * 2) / num_of_boxes
-    line_height = (window.height - padding * 2) / num_of_boxes
-    w = padding
-    h = padding
-    last_w = w + line_width
-    last_h = h + line_height
-    while h <= window.height:
-        while w <= window.width:
-            line1 = Line(Point(last_w, h), Point(w, h))
-            line2 = Line(Point(w, last_h), Point(w, h))
-            print(f"L1:  p1: x: {line1.p1.x}  y: {line1.p1.y}  p2: x: {line1.p2.x}  y: {line1.p2.y}")
-            print(f"L1:  p1: x: {line2.p1.x}  y: {line2.p1.y}  p2: x: {line2.p2.x}  y: {line2.p2.y}")
-            window.draw_line(line1, "black")
-            window.draw_line(line2, "red")
-
-            last_w = w
-            w += line_width
+#            last_w = w
+ #           w += line_width
         
-        last_w = padding
-        w = last_w
+#        last_w = padding
+ #       w = last_w
 
-        h += line_height
+  #      h += line_height
     # ^^^ temp code block ^^^
+
+    cell_size = 100
+    half_cell = cell_size / 2
+    center_x = window.width / 2
+    center_y = window.height / 2
+    cell1 = Cell(window, Point(
+            center_x - half_cell,
+            center_y - half_cell
+        ), Point(
+            center_x + half_cell,
+            center_y + half_cell
+        ))
+    cell1.draw()
 
 
     window.wait_for_close()
---
+++ core/objects/cell.py
@@ -0,0 +1,27 @@
+# a cell in the maze
+from objects.window import Window
+from objects.line import Line
+from objects.point import Point
+
+class Cell:
+    def __init__(self, window:Window, p1:Point, p2:Point):
+        self.__win = window
+
+        self.p1 = p1
+        self.p2 = p2
+
+        self.left = Line(Point(self.p1.x, self.p1.y), Point(self.p1.x, self.p2.y))
+        self.right = Line(Point(self.p2.x, self.p1.y), Point(self.p2.x, self.p2.y))
+        self.top = Line(Point(self.p1.x, self.p1.y), Point(self.p2.x, self.p1.y))
+        self.bottom = Line(Point(self.p1.x, self.p2.y), Point(self.p2.x, self.p2.y))
+    
+    def draw(self):
+        if self.left:
+            self.__win.draw_line(self.left, "black")
+        if self.right:
+            self.__win.draw_line(self.right, "black")
+        if self.top:
+            self.__win.draw_line(self.top, "red")
+        if self.bottom:
+            self.__win.draw_line(self.bottom, "red")
+        
