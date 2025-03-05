fae6705ae3c8a465265fef206713e4f111ee0eb2
Gre-mie <meow31-cat@outlook.com>
2025-03-05 20:59:58 +0000

    Add: Maze class with constructer

--- core/main.py
+++ core/main.py
@@ -2,55 +2,62 @@
 
 from objects.window import Window
 from objects.point import Point
-from objects.cell import Cell
+# from objects.cell import Cell
+from objects.maze import Maze
 
 
 def main():
     
     window = Window(800, 600)
 
+    maze = Maze(window, Point(10, 10))
+    
+    print(maze, "\n")
+    for v in maze.debug_formatted_arguments():
+        print(v)
+
 
     # vvv temp code block vvv
 
     # determins the size of the cells and how many rows columns to create
-    cells = []
-    num_cells = 9
-    size_x = window.width // num_cells
-    size_y = window.height // num_cells
+#    cells = []
+ #   num_cells = 9
+  #  size_x = window.width // num_cells
+   # size_y = window.height // num_cells
 
     # creates the cells
 
-    for j in range(0, window.height, size_y):
-        if len(cells) > num_cells - 1:
-            break
-        row = []
-        for i in range(0, window.width, size_x):
-            if len(row) > num_cells -1:
-                break
-            row.append(Cell(
-                window,
-                Point(i, j),
-                Point(i + size_x, j + size_y)
-            ))
-        cells.append(row)
+#    for j in range(0, window.height, size_y):
+ #       if len(cells) > num_cells - 1:
+  #          break
+   #     row = []
+    #    for i in range(0, window.width, size_x):
+     #       if len(row) > num_cells -1:
+      #          break
+       #     row.append(Cell(
+        #        window,
+         #       Point(i, j),
+          #      Point(i + size_x, j + size_y)
+           # ))
+#        cells.append(row)
 
     # prints all cells in list
-    for row in cells:
-        for cell in row:
-            cell.draw()
+ #   for row in cells:
+  #      for cell in row:
+   #         cell.draw()
 
     # testing drawing path function
-    cells[3][3].draw_move(cells[3][4])
-    cells[3][4].draw_move(cells[3][5])
-    cells[3][5].draw_move(cells[4][5], redo=True)
-    cells[4][5].draw_move(cells[5][5], redo=True)
-    cells[5][5].draw_move(cells[5][4])
-    cells[5][4].draw_move(cells[5][3])
-    cells[5][3].draw_move(cells[4][3], redo=True)
-    cells[4][3].draw_move(cells[3][3], redo=True)
+#    cells[3][3].draw_move(cells[3][4])
+ #   cells[3][4].draw_move(cells[3][5])
+  #  cells[3][5].draw_move(cells[4][5], redo=True)
+   # cells[4][5].draw_move(cells[5][5], redo=True)
+    #cells[5][5].draw_move(cells[5][4])
+#    cells[5][4].draw_move(cells[5][3])
+ #   cells[5][3].draw_move(cells[4][3], redo=True)
+  #  cells[4][3].draw_move(cells[3][3], redo=True)
 
 
-    print(f"cells:\ncols: {len(cells[1])}\nrows: {len(cells)}")
+#    print(f"cells:\ncols: {len(cells[1])}\nrows: {len(cells)}")
 
     # ^^^ temp code block ^^^
 
---
+++ core/objects/maze.py
@@ -0,0 +1,45 @@
+# Maze object to hold and print the cells at a see-able speed
+import time
+from objects.cell import Cell
+from objects.point import Point
+
+class Maze:
+    def __init__(
+            self, 
+            win,  
+            origin:Point, 
+            *, 
+            num_rows:int=9, 
+            num_cols:int=9, 
+            cell_size_x:int=47, # size 50 - line width (3)
+            cell_size_y:int=47  # size 50 - line width (3)
+        ):
+
+        self.__origin = origin
+
+        self.rows = num_rows
+        self.cols = num_cols
+        self.cell_x = cell_size_x
+        self.cell_y = cell_size_y
+
+        self.cells = []
+
+        self.__win = win
+
+
+    # temp method
+    def debug_formatted_arguments(self):
+        return [
+            "Class Maze:",
+            f"origin: {self.__origin}",
+            f"rows: {self.rows}",
+            f"cols: {self.cols}",
+            f"cell x: {self.cell_x}",
+            f"cell y: {self.cell_y}",
+            f"cells: {self.cells}",
+            f"win: {self.__win}",
+        ]
+
+    
+    def __repr__(self):
+        return f"Class: Maze(self, win={self.__win}, cords={self.__origin}, num_rows={self.rows}, num_cols={self.cols}, cell_size_x={self.cell_x}, cell_size_y={self.cell_y})"
