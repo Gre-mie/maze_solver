ebb15e403f0d57dae96d9ba76d15ea52eae82a8f
Gre-mie <meow31-cat@outlook.com>
2025-03-05 05:29:30 +0000

    Add: Line object beginning methods

--- core/main.py
+++ core/main.py
@@ -1,13 +1,20 @@
 # main.py, runs the programme
 
 from objects.window import Window
+from objects.line import Line
+from objects.point import Point
 
 def main():
     
     window = Window(800, 600)
-    window.wait_for_close()
-    
 
+    line = Line(Point(0, 0), Point(100, 100))
+    line.draw(window.canvas, "red")
+
+
+
+
+    window.wait_for_close()
 
 
 if __name__ == "__main__":
---
+++ core/objects/line.py
@@ -0,0 +1,11 @@
+# Draws a line using two Point objects
+from tkinter import Canvas
+from objects.point import Point
+
+class Line:
+    def __init__(self, p1:Point, p2:Point):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas:Canvas, col):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=col, width=5)
