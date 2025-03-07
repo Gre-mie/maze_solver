6492bae192cc2cfb10806e3b417384d9cb807959
Gre-mie <meow31-cat@outlook.com>
2025-03-07 07:08:28 +0000

    Update: with more tests

--- core/tests.py
+++ core/tests.py
@@ -12,17 +12,31 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(maze._cells), cols)
         self.assertEqual(len(maze._cells[0]), rows)
     
+    
     def test_maze_row_column_creation_odd(self):
         cols = 3
         rows = 5
         maze = Maze(num_cols=cols, num_rows=rows,)
         self.assertEqual(len(maze._cells), cols)
         self.assertEqual(len(maze._cells[0]), rows)
-    
-    
-    
 
 
+    def test_maze_row_column_creation_zero_val_cols(self):
+        cols = 0
+        rows = 5
+        maze = Maze(num_cols=cols, num_rows=rows)
+        self.assertEqual(maze._cells, [])
+    
 
+    def test_maze_row_column_creation_zero_val_rows(self):
+        cols = 5
+        rows = 0
+        maze = Maze(num_cols=cols, num_rows=rows)
+        self.assertEqual(len(maze._cells), cols)
+
+        for col in maze._cells:
+            self.assertEqual(col, [])
+    
+    
 if __name__ == "__main__":
     unittest.main()
